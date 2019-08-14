8645508be24c559c7f99fcaa94079790e08ea583
Benjamin Iversen <benj590c@edu.campusvejle.dk>
2019-08-14 10:50:58 +0200

    Add project files.

---
+++ benjamin opgeva3/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+
+namespace benjamin_opgeva3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,1);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,2);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,3);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,4);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,5);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,6);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,7);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,8);
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,9);
+
+            Console.Write("Dette er en rulletekst");
+            Thread.Sleep(500);
+            Console.Clear();
+
+            Console.SetCursorPosition(1,10);
+
+
+
+
+
+            Console.ReadKey();
+
+        }
+    }
+}
